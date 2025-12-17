b011ab7399530f07a7e3b1ff4243d9fcdb7f0da6
Jakub Florkowski <jakub.florkowski@nbcuni.com>
2025-12-17 14:26:19 +0100

    uoyuio

---
+++ src/Controls/samples/Controls.Sample.Sandbox/AccessibilityFocusStore.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Maui.Handlers;
+#if ANDROID
+using Android.Views;
+#elif IOS
+using Foundation;
+using UIKit;
+#endif
+
+namespace Maui.Controls.Sample;
+
+public static class AccessibilityFocusStore
+{
+	private static Page? _currentPage;
+	
+
+	public static MauiAppBuilder EnableFocusTracking(this MauiAppBuilder mauiAppBuilder)
+	{
+#if ANDROID
+		ViewHandler.ViewMapper.AppendToMapping("Tracking", (handler, view) =>
+		{
+			if (handler.PlatformView is Android.Views.View androidView)
+				androidView.SetAccessibilityDelegate(new TrackingAccessibilityDelegate());
+		});
+#elif IOS
+		NSNotificationCenter.DefaultCenter.AddObserver(
+			new NSString("UIAccessibilityElementFocusedNotification"),
+			notification =>
+			{
+				if (notification?.UserInfo?["UIAccessibilityFocusedElementKey"] is UIView focusedView)
+				{
+					Console.WriteLine($"VoiceOver focused on: {focusedView}");
+					Remember(focusedView);
+				}
+			});
+#endif
+		return mauiAppBuilder;
+	}
+
+	public static void RestoreFocus(Page currentPage)
+	{
+		_currentPage = currentPage;
+
+#if ANDROID
+		if (_lastFocusedViewOnPage.TryGetValue(_currentPage, out var view) && view is not null)
+		{
+			view.PostDelayed(() =>
+			{
+				Console.WriteLine("Restoring VoiceOver focus to: "+view);
+				view.SendAccessibilityEvent(Android.Views.Accessibility.EventTypes.ViewHoverEnter);
+			}, 300);
+		}
+#elif IOS
+		if (_lastFocusedViewOnPage.TryGetValue(_currentPage, out var uiView) && uiView is not null)
+		{
+			Console.WriteLine("Restoring VoiceOver focus to: "+uiView);
+			UIAccessibility.PostNotification(UIAccessibilityPostNotification.ScreenChanged, uiView);
+		}
+#endif
+	}
+
+	public static void StopTracking()
+	{
+		_currentPage = null;
+	}
+
+#if ANDROID
+	private static Dictionary<Page, Android.Views.View> _lastFocusedViewOnPage = new();
+
+	private static void Remember(Android.Views.View nativeView)
+	{
+		if (nativeView is null || _currentPage is null)
+			return;
+
+		_lastFocusedViewOnPage[_currentPage] = nativeView;
+	}
+
+#elif IOS
+	private static Dictionary<Page, UIView> _lastFocusedViewOnPage = new();
+
+	private static void Remember(UIView nativeView)
+	{
+		if (nativeView is null || _currentPage is null)
+			return;
+
+		_lastFocusedViewOnPage[_currentPage] = nativeView;
+	}
+#endif
+
+#if ANDROID
+	class TrackingAccessibilityDelegate : Android.Views.View.AccessibilityDelegate
+	{
+		public override void SendAccessibilityEvent(Android.Views.View host, Android.Views.Accessibility.EventTypes eventType)
+		{
+			base.SendAccessibilityEvent(host, eventType);
+
+			if (eventType == Android.Views.Accessibility.EventTypes.ViewHoverEnter ||
+			 	eventType == Android.Views.Accessibility.EventTypes.ViewFocused)
+				Remember(host);
+		}
+	}
+#endif
+}
+
--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.Maui.Handlers;
-#if ANDROID
-using Android.Views;
-#elif IOS
-using Foundation;
-using UIKit;
-#endif
-
-namespace Maui.Controls.Sample;
+﻿namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
@@ -42,16 +34,13 @@ public partial class MainPage : ContentPage
 	protected override async void OnAppearing()
 	{
 		base.OnAppearing();
-
 		AccessibilityFocusStore.RestoreFocus(this);
-		// await Task.Delay(300);
-		// AccessibilityFocusStore.RestoreFocus();
-
 	}
 
-	protected override async void OnDisappearing()
+	protected override void OnDisappearing()
 	{
 		base.OnDisappearing();
+		AccessibilityFocusStore.StopTracking();
 	}
 
 	class Subpage1 : ContentPage
@@ -81,6 +70,12 @@ public partial class MainPage : ContentPage
 			AccessibilityFocusStore.RestoreFocus(this);
 		}
 
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			AccessibilityFocusStore.StopTracking();
+		}
+
 		class Subpage2 : ContentPage
 		{
 			public Subpage2()
@@ -88,95 +83,19 @@ public partial class MainPage : ContentPage
 				Title = "Subpage 2";
 				Content = new Label { Text = "This is Subpage 2" };
 			}
+
+			protected override async void OnAppearing()
+			{
+				base.OnAppearing();
+				AccessibilityFocusStore.RestoreFocus(this);
+			}
+
+			protected override void OnDisappearing()
+			{
+				base.OnDisappearing();
+				AccessibilityFocusStore.StopTracking();
+			}
 		}
 	}
 
 }
-
-public static class AccessibilityFocusStore
-{
-	private static Page? _currentPage;
-
-	public static MauiAppBuilder EnableFocusTracking(this MauiAppBuilder mauiAppBuilder)
-	{
-#if ANDROID
-		ViewHandler.ViewMapper.AppendToMapping("Tracking", (handler, view) =>
-		{
-			if (handler.PlatformView is Android.Views.View androidView)
-				androidView.SetAccessibilityDelegate(new TrackingAccessibilityDelegate());
-		});
-#elif IOS
-		NSNotificationCenter.DefaultCenter.AddObserver(
-			new NSString("UIAccessibilityElementFocusedNotification"),
-			notification =>
-			{
-				if (notification?.UserInfo?["UIAccessibilityFocusedElementKey"] is UIView focusedView)
-				{
-					Console.WriteLine($"VoiceOver focused on: {focusedView}");
-					Remember(focusedView);
-				}
-			});
-#endif
-		return mauiAppBuilder;
-	}
-
-	public static void RestoreFocus(Page currentPage)
-	{
-		_currentPage = currentPage;
-
-#if ANDROID
-		if (_lastFocusedViewOnPage.TryGetValue(_currentPage, out var view) && view is not null)
-		{
-			view.PostDelayed(() =>
-			{
-				Console.WriteLine("Restoring VoiceOver focus to: "+view);
-				view.SendAccessibilityEvent(Android.Views.Accessibility.EventTypes.ViewHoverEnter);
-			}, 300);
-		}
-#elif IOS
-		if (_lastFocusedViewOnPage.TryGetValue(_currentPage, out var uiView) && uiView is not null)
-		{
-			Console.WriteLine("Restoring VoiceOver focus to: "+uiView);
-			UIAccessibility.PostNotification(UIAccessibilityPostNotification.ScreenChanged, uiView);
-		}
-#endif
-	}
-
-#if ANDROID
-	private static Dictionary<Page, Android.Views.View> _lastFocusedViewOnPage = new();
-
-	private static void Remember(Android.Views.View nativeView)
-	{
-		if (nativeView is null || _currentPage is null)
-			return;
-
-		_lastFocusedViewOnPage[_currentPage] = nativeView;
-	}
-
-#elif IOS
-	private static Dictionary<Page, UIView> _lastFocusedViewOnPage = new();
-
-	private static void Remember(UIView nativeView)
-	{
-		if (nativeView is null || _currentPage is null)
-			return;
-
-		_lastFocusedViewOnPage[_currentPage] = nativeView;
-	}
-#endif
-
-#if ANDROID
-	class TrackingAccessibilityDelegate : Android.Views.View.AccessibilityDelegate
-	{
-		public override void SendAccessibilityEvent(Android.Views.View host, Android.Views.Accessibility.EventTypes eventType)
-		{
-			base.SendAccessibilityEvent(host, eventType);
-
-			if (eventType == Android.Views.Accessibility.EventTypes.ViewHoverEnter ||
-			 	eventType == Android.Views.Accessibility.EventTypes.ViewFocused)
-				Remember(host);
-		}
-	}
-#endif
-}
-
